993afcc547cf3497176d3e71bb68393eb1629a01
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2022-05-13 16:09:04 -0500

    Set up a spring boot application with appropriate dependencies.
    The dependencies include JDBC, JPA, Web, and H2 for our h2-in-memory database.

---
+++ spring-jdc-jpa/src/main/java/com/eriaothienopinyi/springjdcjpa/SpringJdcJpaApplication.java
@@ -0,0 +1,13 @@
+package com.eriaothienopinyi.springjdcjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class  SpringJdcJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJdcJpaApplication.class, args);
+	}
+
+}
---
+++ spring-jdc-jpa/src/test/java/com/eriaothienopinyi/springjdcjpa/SpringJdcJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eriaothienopinyi.springjdcjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJdcJpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
