f9649b35387ef029dc1a0d2851edbe002adf518c
Eria Othieno Pinyi <pinyito@users.noreply.github.com>
2022-05-13 18:12:06 -0500

    Implemented a DAO method to retrieve data using spring JDBC (JdbcTemplate)
    Spring JDBC offer 2 crucial advantages over traditional JDBC. Less amount of code required and less mistakes expected.

--- spring-jdc-jpa/src/main/java/com/eriaothienopinyi/springjdcjpa/SpringJdcJpaApplication.java
+++ spring-jdc-jpa/src/main/java/com/eriaothienopinyi/springjdcjpa/SpringJdcJpaApplication.java
@@ -1,13 +1,26 @@
 package com.eriaothienopinyi.springjdcjpa;
 
+import com.eriaothienopinyi.springjdcjpa.JDBCRepository.PersonJdbcDao;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  SpringJdcJpaApplication {
+public class  SpringJdcJpaApplication implements CommandLineRunner {
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@Autowired
+	PersonJdbcDao dao;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringJdcJpaApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		logger.info("{}",dao.findAll());
+	}
 }
